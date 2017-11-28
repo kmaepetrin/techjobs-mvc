d3cc5a7c3627338e0574062a86441d9ef7ac2615
K Mae Petrin <kmaepetrin@gmail.com>
2017-11-27 20:17:41 -0600

    Added search functionality.

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -22,6 +23,22 @@ public class SearchController {
         return "search";
     }
 
-    // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results", method = RequestMethod.GET)
+    public String search(@RequestParam String searchType, @RequestParam String searchTerm, Model model) {
+
+        ArrayList<HashMap<String, String>> jobs;
+
+        if (searchType == "all" || searchTerm == "") {
+            jobs = JobData.findAll();
+        } else if (searchType == "all" && searchTerm != "") {
+            jobs = JobData.findByValue(searchTerm);
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("jobs", jobs);
+        return "search";
+    }
 
 }
